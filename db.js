168cfd2dea678f638cf989a7c2f1c4fe44e07c1c
Asif mia <asifmia.dev26@gmail.com>
2026-04-18 11:43:04 +0600

    Complete CRUD with mongoDB using JavaScript

--- db.js
+++ db.js
@@ -19,6 +19,19 @@ mongoose.connect("mongodb://localhost:27017/testDB",{
       const users = await getAllUsers();
       console.log(users);
 
+      const user = await findUserById('69e27e07cc6dd5e5781ad556');
+      console.log('Find By Id ->',user);
+
+
+      const user2 = await findByName('Foysal');
+      console.log('Find By Name -> ',user2);
+
+      const updatedUser = await UpdateUserByName('Foysal','0194598423');
+      console.log('Updated User ->',updatedUser);
+
+      const deltedUser = await deleteUserById('69e27e07cc6dd5e5781ad556');
+      console.log('Deleted User -> ',deltedUser)
+
       mongoose.connection.close(true);
 
 }).catch((e)=>{
@@ -57,8 +70,33 @@ async function createUser(data){
 }
 
 async function getAllUsers(){
-  const users = User.find();
+  const users = await User.find();
   return users;
 }
 
+async function findUserById(id){
+  const user = await User.findById(id);
+  return user;
+}
+
+async function findByName(name){
+  const user = await User.findOne({name:name});
+  return user;
+}
+
+async function UpdateUserByName(name,newPhone){
+  const updatedUser = await User.findOneAndUpdate(
+    {name:name},
+    {phone:newPhone},
+    {returnDocument:"after"}
+  );
+
+  return updatedUser;
+  
+}
+
+async function deleteUserById(id) {
+  const user = await User.findOneAndDelete({ _id: id });
+  return user;
+}
 
